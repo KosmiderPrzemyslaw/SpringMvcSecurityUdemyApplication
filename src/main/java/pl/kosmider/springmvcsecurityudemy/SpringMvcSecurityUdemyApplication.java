864b1ca8e9b3e989289a8e9551806fb8bb9b14ea
KosmiderPrzemyslaw <kosmiderprzemyslaw91@gmail.com>
2024-01-19 06:00:25 +0100

    create project spring boot mvc security

---
+++ src/main/java/pl/kosmider/springmvcsecurityudemy/SpringMvcSecurityUdemyApplication.java
@@ -0,0 +1,13 @@
+package pl.kosmider.springmvcsecurityudemy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringMvcSecurityUdemyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringMvcSecurityUdemyApplication.class, args);
+    }
+
+}
---
+++ src/test/java/pl/kosmider/springmvcsecurityudemy/SpringMvcSecurityUdemyApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.kosmider.springmvcsecurityudemy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringMvcSecurityUdemyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
